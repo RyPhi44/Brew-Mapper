ecf24ce311e360b2f76db0ed8930e4f8ce77ec43
Ryan Phillips <rphillips4417@gmail.com>
2017-02-07 14:35:40 -0700

    Updated theme, added linked pages, stylized

---
+++ app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+$.get("http://api.brewerydb.com/v2/?key=f16dad2851cec7f5e563c75bc9a760b1", function(result) {
+findBrews(resutl);
+});
+
+function findBrews(beerList);
+
+
+
+
+
+
+}
