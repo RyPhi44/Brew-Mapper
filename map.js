3960790ff1e6f0b123dd84945c3816aeaa6fc0b1
Ryan Phillips <rphillips4417@gmail.com>
2017-02-09 14:35:56 -0700

    Added clear-markers functionality

--- map.js
+++ map.js
@@ -1,41 +1,55 @@
-var map, infoWindows = [];
+var map, infoWindows = [], markers = [];
 $(document).ready(function(){
 
 // sends request to brewerydb for information on breweries
-  $.get("http://galvanize-cors-proxy.herokuapp.com/http://api.brewerydb.com/v2/locations/?key=f16dad2851cec7f5e563c75bc9a760b1&region=colorado&locationType=micro,macro,nano,cidery", function(result) {
-
-console.log(result);
-    result.data.forEach(function(brewery) {
-      // $("ul").append("<li>"+brewery.brewery.name+"</li>")
-      var marker = new google.maps.Marker({
-        position: {lat: brewery.latitude, lng: brewery.longitude},
-        map: map
-      });
-      var content = "<h1 class='nameTag'>"+brewery.brewery.name+"</h1>" +
-        "<p class='markerTag'>"+brewery.streetAddress+", "+brewery.locality+", "+brewery.region+" "+brewery.postalCode+"</p>"
-      if (brewery.phone !== undefined) {
-        content += "<p class='phoneTag'>"+"Phone: "+brewery.phone+"</p>" ;
+    function findData(postalCode){
+      postalCode = postalCode || 80202
+      for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null)
       }
-      if (brewery.website !== undefined) {
-        content += "<p class='webTag'>"+brewery.website+"</p>" ;
-      }
-
-      var infowindow = new google.maps.InfoWindow({
-        content,
-        maxWidth: 300
-      });
-
-      infoWindows.push(infowindow);
-
-      marker.addListener('click', function() {
-        for (var i = 0; i < infoWindows.length; i++) {
-          infoWindows[i].close()
+      $.get("http://galvanize-cors-proxy.herokuapp.com/http://api.brewerydb.com/v2/locations/?key=f16dad2851cec7f5e563c75bc9a760b1&postalCode="+postalCode+"&locationType=micro,macro,nano,cidery", function(result) {
+        console.log(result);
+      result.data.forEach(function(brewery) {
+        // $("ul").append("<li>"+brewery.brewery.name+"</li>")
+        var marker = new google.maps.Marker({
+          position: {lat: brewery.latitude, lng: brewery.longitude},
+          map: map
+        });
+        markers.push(marker)
+        var content = "<h1 class='nameTag'>"+brewery.brewery.name+"</h1>" +
+          "<p class='markerTag'>"+brewery.streetAddress+", "+brewery.locality+", "+brewery.region+" "+brewery.postalCode+"</p>"
+        if (brewery.phone !== undefined) {
+          content += "<p class='phoneTag'>"+"Phone: "+brewery.phone+"</p>" ;
         }
-        infowindow.open(map, marker);
+        if (brewery.website !== undefined) {
+          content += "<a class='webTag' href="+brewery.website+">"+brewery.website+"</a>";
+        }
+
+        var infowindow = new google.maps.InfoWindow({
+          content,
+          maxWidth: 300
+        });
+
+        infoWindows.push(infowindow);
+
+        marker.addListener('click', function() {
+          for (var i = 0; i < infoWindows.length; i++) {
+            infoWindows[i].close()
+          }
+          infowindow.open(map, marker);
+
+        });
+
       });
 
-    });
+    })
+  }
+  document.getElementById("location_button").addEventListener("click", function(){
+    // find input box, get value, store variable, pass to findData jquery select input box
+    var zipData = $(".zipInput").val()
+    findData(zipData)
   })
+
 });
 
 function initMap() {
@@ -45,7 +59,7 @@ function initMap() {
     center: denver
   });
 
-  document.getElementById("location_button").addEventListener("click", result);
+
 
 }
 
